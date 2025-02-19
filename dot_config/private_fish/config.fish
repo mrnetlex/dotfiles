ae54484969123d7b5da9d9f9ea059c5f8e80fcc9
netlex <mrnetlex@gmail.com>
2025-02-19 22:35:28 +0100

    added fonts to kitty, brought back flatpak fix

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -134,6 +134,16 @@ if status is-interactive
              end
              rm -f -- "$tmp"
         end
+
+         #Flatpak fix
+         set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+         set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+         for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+             if test -d $flatpakdir
+                     contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+             end
+         end
         
     ##  Themes
          #TokyoNight Color Palette
