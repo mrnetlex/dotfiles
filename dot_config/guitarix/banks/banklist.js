5eb6e964b3d5c1633e5f44c2e5300cf2f6ff9aaa
netlex <mrnetlex@gmail.com>
2025-10-20 21:51:36 +0200

    guitarix banks that i found sound nice

---
+++ dot_config/guitarix/banks/banklist.js
@@ -0,0 +1,13 @@
+[
+  ["babiskouvalis", "babiskouvalis.gx", 1, 0, [1, 2], 1759778939],
+  ["musiclab-AngusYoung", "musiclab-AngusYoung.gx", 1, 0, [1, 2], 1759777053],
+  ["Gritty_Clean", "Gritty_Clean.gx", 1, 0, [1, 2], 1759618834],
+  ["musiclab-MarkKnopfler", "musiclab-MarkKnopfler.gx", 1, 0, [1, 2], 1758719905],
+  ["Joe2-1", "Joe2-1.gx", 1, 0, [1, 2], 1758649201],
+  ["musiclab-EricClapton", "musiclab-EricClapton.gx", 1, 0, [1, 2], 1758719860],
+  ["musiclab-DavidGilmour", "musiclab-DavidGilmour.gx", 1, 0, [1, 2], 1758648970],
+  ["Boogie", "Boogie.gx", 1, 0, [1, 2], 1758648237],
+  ["AJFA", "AJFA.gx", 1, 0, [1, 2], 1758648226],
+  ["cleansounds", "cleansounds.gx", 1, 0, [1, 2], 1758600041],
+  ["Scratchpad", "scratchpad.gx", 0, 0, [1, 2], 1758599169]
+]
